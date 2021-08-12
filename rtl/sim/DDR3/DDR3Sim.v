103e88b66fece04edfdc6177def05623920a54e3
Stéphane Chevigny <infphyny@gmail.com>
2021-08-12 09:10:49 -0400

    Added stub for intel DDR3 UniPhy ip for verilator simulator

---
+++ rtl/sim/DDR3/DDR3Sim.v
@@ -0,0 +1,67 @@
+// Generator : SpinalHDL v1.6.0    git head : 73c8d8e2b86b45646e9d0b2e729291f2b65e6be3
+// Component : DDR3Sim
+// Git hash  : d67efa0252e13955ba4602fde96281d4b343804d
+
+
+
+module DDR3Sim (
+  output              avl_ready,
+  input               avl_burst_begin,
+  output              avl_rdata_valid,
+  input      [25:0]   avl_addr,
+  output     [63:0]   avl_rdata,
+  input      [63:0]   avl_wdata,
+  input      [7:0]    avl_be,
+  input               avl_read_req,
+  input               avl_write_req,
+  input      [2:0]    avl_size,
+  input               clk,
+  input               reset
+);
+  reg        [63:0]   _zz_ram_port1;
+  reg                 _zz_1;
+  reg                 rdata_valid;
+  reg        [63:0]   rdata;
+  wire                when_ddr3_l48;
+  wire                when_ddr3_l53;
+  reg [63:0] ram [0:67108863];
+
+  always @(posedge clk) begin
+    if(_zz_1) begin
+      ram[avl_addr] <= avl_wdata;
+    end
+  end
+
+  always @(posedge clk) begin
+    if(avl_read_req) begin
+      _zz_ram_port1 <= ram[avl_addr];
+    end
+  end
+
+  always @(*) begin
+    _zz_1 = 1'b0;
+    if(when_ddr3_l48) begin
+      _zz_1 = 1'b1;
+    end
+  end
+
+  assign avl_rdata = rdata;
+  assign avl_rdata_valid = rdata_valid;
+  assign avl_ready = 1'b1;
+  assign when_ddr3_l48 = (avl_write_req == 1'b1);
+  assign when_ddr3_l53 = (avl_read_req == 1'b1);
+  always @(posedge clk or posedge reset) begin
+    if(reset) begin
+      rdata_valid <= 1'b0;
+      rdata <= 64'h0;
+    end else begin
+      rdata_valid <= 1'b0;
+      if(when_ddr3_l53) begin
+        rdata_valid <= 1'b1;
+        rdata <= _zz_ram_port1;
+      end
+    end
+  end
+
+
+endmodule
---
+++ rtl/sim/DDR3/src/main/scala/ddr3/ddr3.scala
@@ -0,0 +1,69 @@
+/*
+  DDR3 avalon bus simulation code to replace UniPhy ddr3 memory when simulating with verilog
+
+  TODO implement byte enable
+ */
+
+package ddr3_sim
+
+import spinal.core._
+import spinal.lib._
+
+
+
+//Hardware definition
+class DDR3Sim extends Component {
+  val WORDS_COUNT = 67108864
+  val io = new Bundle {
+    val avl_ready = out Bool()
+    val avl_burst_begin = in Bool()
+    val avl_rdata_valid = out Bool()
+    val avl_addr = in UInt(log2Up(WORDS_COUNT) bits)
+    val avl_rdata = out Bits(64 bits)
+    val avl_wdata = in Bits(64 bits)
+    val avl_be = in Bits(8 bits)
+    val avl_read_req = in Bool()
+    val avl_write_req = in Bool()
+    val avl_size = in UInt(3 bits) 
+  }
+ 
+  noIoPrefix
+
+ val rdata_valid = Reg(Bool()) init False
+ //val rdata = Reg(Bits(64 bits)) init 0
+ //io.avl_rdata := rdata
+ io.avl_rdata_valid := rdata_valid
+ rdata_valid := False
+
+ io.avl_ready := True
+
+ 
+ io.avl_rdata := 0
+ 
+
+ val ram = Mem(Bits(64 bits),WORDS_COUNT)
+
+  
+  when(io.avl_write_req === True)
+  {
+    ram.write(io.avl_addr,io.avl_wdata)
+  }
+   
+  when(io.avl_read_req === True)
+  {
+    rdata_valid := True
+    io.avl_rdata := ram.readSync(io.avl_addr,io.avl_read_req)
+  }
+
+
+
+}
+
+
+//Generate the MyTopLevel's Verilog
+object DDR3SimVerilog {
+  def main(args: Array[String]) {
+    SpinalConfig().withPrivateNamespace.generateVerilog(new DDR3Sim).printPruned()
+ //   SpinalVerilog(new AvalonClockDomainCrossingBridge).printPruned()
+  }
+}
